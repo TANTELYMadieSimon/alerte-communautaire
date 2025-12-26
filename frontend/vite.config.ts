128149b5074f8e24763b198dc6f542de92038cfc
TANTELY MADIE Simon <simonmadie2006@gmail.com>
2025-12-26 19:34:55 +0300

    Update frontend for Vercel

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  define: {
+    'process.env.BACKEND_URL': JSON.stringify('https://alerte-communautaire.onrender.com')
+  }
 })
