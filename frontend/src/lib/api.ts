5e249283480e428bea144d63721218fa6b7adeee
TANTELY MADIE Simon <simonmadie2006@gmail.com>
2025-12-28 07:14:11 +0300

    Fix API URL

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -3,3 +3,18 @@ export const API_URL = import.meta.env.VITE_API_URL;
 export const ALERTES_API_URL = `${API_URL}/alertes/`;
 export const ANNONCES_API_URL = `${API_URL}/annonces/`;
 export const USERS_API = `${API_URL}/users`;
+export const loginUser = async (data: any) => {
+  const res = await fetch(`${API_URL}/api/login/`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  });
+
+  if (!res.ok) {
+    throw new Error("Erreur API");
+  }
+
+  return res.json();
+};
